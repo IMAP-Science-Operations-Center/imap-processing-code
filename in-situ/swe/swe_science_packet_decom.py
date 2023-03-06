cc486b088d6879a9099b9b41ca0244faceff8837
Tenzin Choedon <tenzin.choedon@lasp.colorado.edu>
2023-03-06 11:30:00 -0700

    changed CCSDS hearder element to match what's required in lasp_packet.

---
+++ in-situ/swe/swe_science_packet_decom.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+import bitstring
+from lasp_packets import xtcedef, parser
+
+
+packet_file = Path('science_block_20221116_163611Z_idle.bin')
+with packet_file.open(mode='rb') as fh:
+    binary_data = bitstring.ConstBitStream(fh)
+    
+xtce_document = Path('swe_packet_definition.xml')
+packet_definition = xtcedef.XtcePacketDefinition(xtce_document)
+
+# Initialize parser with custom XTCE definition
+my_parser = parser.PacketParser(packet_definition)
+
+# Generator object that spits out parsed Packet objects containing header and data attributes
+packet_generator = my_parser.generator(binary_data)
+
+for packet in packet_generator:
+    # Do something with the packet data
+    print(packet.header['PKT_APID'])
+    # print(packet.data)
